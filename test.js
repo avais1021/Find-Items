a07721efa6198a3c8e26e1921687db8ffafba410
Owes Ansari <Owes.Ansari@teknopoint.local>
2023-10-09 12:53:50 +0530

    new changes

--- test.js
+++ test.js
@@ -8,11 +8,28 @@ let save = document.querySelector('#save');
 let htmlitems = document.querySelector('.items');
 let box = document.querySelector('.box');
 let search_input = document.querySelector('#search_input');
+let searchIcon = document.querySelector('.fa-magnifying-glass');
 let search_btn = document.querySelector('#search_btn');
 let ArrayObj = JSON.parse(localStorage.getItem('appdata')) || [];
 
 // var counter = 0;
 
+console.log(ArrayObj, 'ArrayObj');
+
+// ArrayObj.forEach((ele) => {
+
+
+//     function objectAreEqual(obj, item) {
+//         return JSON.stringify(obj) === JSON.stringify(item);
+//     }
+
+//     const removeDublicateItems = ele.userItems.filter((item, index, self) => {
+//         return self.findIndex(obj => objectAreEqual(obj, item)) === index;
+//     })
+
+//     console.log(removeDublicateItems, 'removeDublicateItems');
+
+// })
 
 addParent.addEventListener('click', () => {
 
@@ -352,16 +369,18 @@ function commonEditCode(e) {
     const updateItemeName = prompt('edit your Item')
     console.log(updateItemeName)
 
-    ArrayObj.forEach((ele) => {
-        ele.userItems.forEach((item, idx) => {
+    if (containerInfodata != null) {
+        ArrayObj.forEach((ele) => {
+            ele.userItems.forEach((item, idx) => {
 
-            if (updateItemeName !== null) {
-                if (ele.id == containerInfodata && dataEditIcon == idx) {
-                    item.uItems = updateItemeName;
+                if (updateItemeName !== null) {
+                    if (ele.id == containerInfodata && dataEditIcon == idx) {
+                        item.uItems = updateItemeName;
+                    }
                 }
-            }
+            })
         })
-    })
+    }
 
     localStorage.setItem('appdata', JSON.stringify(ArrayObj))
 
@@ -381,9 +400,14 @@ function commonEditCode(e) {
 // })
 
 
-search_input.addEventListener('keyup', (e) => {
+searchIcon.addEventListener('click', (e) => {
 
+    // let timeOut = setTimeout(() => {
     Search()
+    // }, 1000);
+
+    // return () => clearTimeout(timeOut)
+
 
 
 })
@@ -398,7 +422,19 @@ function Search() {
     } else {
 
         ArrayObj.forEach((ele, id2) => {
-            ele.userItems.forEach((item, idx) => {
+
+
+            function objectAreEqual(obj, item) {
+                return JSON.stringify(obj) === JSON.stringify(item);
+            }
+
+            const removeDublicateItems = ele.userItems.filter((item, index, self) => {
+                return self.findIndex(obj => objectAreEqual(obj, item)) === index;
+            })
+
+            console.log(removeDublicateItems, 'removeDublicateItems');
+
+            removeDublicateItems.forEach((item, idx) => {
 
                 var eTargetVal = search_input.value.toLowerCase();
                 // var eTargetVal = e.target.value.toLowerCase()
@@ -414,20 +450,20 @@ function Search() {
                     searchDrLi = ` <li>${item.uItems} <i class="fa-solid fa-pen-to-square edit_item" data-drwItemEdit="${idx}"></i>  <i class="fa-solid fa-trash drwItem" data-drwItemdel="${idx}"></i> </li>`
 
                     serach_drwa += `
-             <div class="containersInfo" data-cninf="${ele.id}">
-             <i class="fa-solid fa-trash draww" data-drwdel="${id2}"></i>
-             <h2>Draw <span>${ele.containerN[0]}</span> <i class="fa-solid fa-pen-to-square edit_Draw" data-drwEdit="${id2}"> </i> </h2>
-             <ul class="items_render">
-                ${searchDrLi}
-              </ul>
-              <div class="addMore"> <input type="text"> <button class="addmorebtn" data-addmrbtndata="${ele.id}"><i class="fa-solid fa-plus"></i></button></div>
-              <p id="allItems" data-allitem="${ele.id}">All items <i class="fa-solid fa-angle-down"></i></p>
-              <ul class="allItem_render" data-allrenderr="${ele.id}">
+                   <div class="containersInfo" data-cninf="${ele.id}">
+                    <i class="fa-solid fa-trash draww" data-drwdel="${id2}"></i>
+                    <h2>Draw <span>${ele.containerN[0]}</span> <i class="fa-solid fa-pen-to-square edit_Draw" data-drwEdit="${id2}"> </i> </h2>
+                     <ul class="items_render">
+                       ${searchDrLi}
+                    </ul>
+                    <div class="addMore"> <input type="text"> <button class="addmorebtn" data-addmrbtndata="${ele.id}"><i class="fa-solid fa-plus"></i></button></div>
+                      <p id="allItems" data-allitem="${ele.id}">All items <i class="fa-solid fa-angle-down"></i></p>
+                      <ul class="allItem_render" data-allrenderr="${ele.id}">
             
-              </ul>
-                  </div>
-               `
-
+                  </ul>
+                           </div>
+                       `
+                    // if(ele.id ==  )
 
                 }
 
@@ -439,7 +475,6 @@ function Search() {
     // console.log('input val', e.target.value)
 
     box.innerHTML = serach_drwa;
-
 
 
     let allItems = document.querySelectorAll('#allItems');
@@ -564,7 +599,7 @@ yourDraw__row.addEventListener('click', () => {
 function drawOption() {
 
     // let htmlStr_AllShow = ''
-    
+
     yourDrawOptionItems();
 
     if (yourDraw__row.classList.contains('open')) {
@@ -575,7 +610,7 @@ function drawOption() {
     draw_nameP.forEach((itemDr) => {
         itemDr.addEventListener('click', (e) => {
 
-           drawOptionOnRendering(itemDr);
+            drawOptionOnRendering(itemDr);
 
         })
     })
@@ -588,7 +623,7 @@ function drawOption() {
 
 }
 
-function yourDrawOptionItems (){
+function yourDrawOptionItems() {
     let htmlStr_drawerName = "";
     ArrayObj.forEach((ele) => {
         if (ele.containerN[0] !== undefined) {
@@ -599,7 +634,7 @@ function yourDrawOptionItems (){
     yourDraw__row1.innerHTML = `<p class="showAllDraw">show All Draws</p> ${htmlStr_drawerName}`;
 }
 
-function drawOptionOnRendering (itemDr){
+function drawOptionOnRendering(itemDr) {
     console.log(itemDr.dataset.drmenu)
 
     let htmlStr_drawoption = '';
@@ -694,8 +729,8 @@ function deleteDrwa(e) {
 
 }
 
-function editDrwNamefun (e) {
-    
+function editDrwNamefun(e) {
+
     // const dataEditIcon = e.target.getAttribute('data-drwItemEdit');
     const containerInfodata = e.target.closest('.containersInfo').getAttribute('data-cninf');
     const containerInfospan = e.target.closest('.containersInfo').querySelector('h2 span');
@@ -706,14 +741,14 @@ function editDrwNamefun (e) {
     console.log(updateItemeName)
 
     ArrayObj.forEach((ele) => {
-       
 
-            if (updateItemeName !== null) {
-                if (ele.id == containerInfodata ) {
-                    ele.containerN[0] = updateItemeName;
-                }
+
+        if (updateItemeName !== null) {
+            if (ele.id == containerInfodata) {
+                ele.containerN[0] = updateItemeName;
             }
-        
+        }
+
     })
 
     containerInfospan.innerHTML = updateItemeName;
@@ -725,7 +760,7 @@ function editDrwNamefun (e) {
     draw_nameP.forEach((itemDr) => {
         itemDr.addEventListener('click', (e) => {
 
-           drawOptionOnRendering(itemDr);
+            drawOptionOnRendering(itemDr);
 
         })
     })
